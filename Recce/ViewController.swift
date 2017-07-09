6d3ecba6e9b006e10a8e5574ca38e18a8cbf80ae
Rhys <rhys.edwards91@gmail.com>
2017-07-09 23:07:55 +0100

    Starting the build of Recce.
    - Added current user location
    - Added photo selection from library
    
    To-do
    - Add photo from camera
    - Save photo
    - Append long.lat data to exif data for each photo

--- Recce/ViewController.swift
+++ Recce/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    // Get the longitude and latitude
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        // THIS WORKS!
+        // Work out how to append this to photo exif, or in a DB with a UID for the photo
+        print(myLocation.latitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+    }
+    
+    // Let a user pick an image (can only select from library)
+    @IBAction func importImage(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        // Display from library
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true) {
+            // After it is complete
+        }
+    }
+    
+    // Display a photo in a view (currently displays from library)
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImageView.image = image
+        } else {
+            // Error message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set up the location manager
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
