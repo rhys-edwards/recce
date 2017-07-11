d5e1e6875c8d81a0c766c2b3cd6ce680d10ac095
Rhys <rhys.edwards91@gmail.com>
2017-07-11 22:43:45 +0100

    Locations and date EXIF data are now accessible.

--- Recce/ViewController.swift
+++ Recce/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 import MapKit
+import Photos
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var geoLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     
     // Get the longitude and latitude
     let manager = CLLocationManager()
@@ -47,16 +51,70 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     
-    // Display a photo in a view (currently displays from library)
+    // GITHUB 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            myImageView.image = image
-        } else {
-            // Error message
+        var chosenImage:UIImage?
+        var location = "Not known"
+        var timeTaken = "Not known"
+        
+        if let URL = info[UIImagePickerControllerReferenceURL] as? URL {
+            print("We got the URL as \(URL)")
+            let opts = PHFetchOptions()
+            opts.fetchLimit = 1
+            let assets = PHAsset.fetchAssets(withALAssetURLs: [URL], options: opts)
+            for assetIndex in 0..<assets.count {
+                let asset = assets[assetIndex]
+                location = String(describing: asset.location)
+                timeTaken = asset.creationDate!.description
+                
+                CLGeocoder().reverseGeocodeLocation(asset.location!, completionHandler: {(placemarks, error) -> Void in
+                    print(location)
+                    
+                    if let error = error {
+                        print("Reverse geocoder failed with error" + error.localizedDescription)
+                        return
+                    }
+                    
+                    if placemarks != nil && placemarks!.count > 0 {
+                        DispatchQueue.main.async {
+                            self.geoLabel.text = placemarks![0].locality!
+                        }
+                    } else {
+                        print("There's a problem with the data, motherfucker")
+                    }
+                })
+            }
         }
         
-        self.dismiss(animated: true, completion: nil)
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            chosenImage = editedImage
+        } else if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            chosenImage = selectedImage
+        }
+        
+        dismiss(animated: true) {
+            DispatchQueue.main.async {
+                self.myImageView.image = chosenImage
+                self.locationLabel.text = location
+                self.timeLabel.text = timeTaken
+            }
+        }
     }
+
+    
+    
+//    // Display a photo in a view (currently displays from library)
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: NSDictionary!) {
+//        let metadata = info[UIImagePickerControllerReferenceURL]!
+//        print(metadata)
+//        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+//            myImageView.image = image
+//        } else {
+//            // Error message
+//        }
+//        
+//        self.dismiss(animated: true, completion: nil)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
