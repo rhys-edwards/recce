888e1713df9b39950ae567921a77e50992eff9b4
Rhys <rhys.edwards91@gmail.com>
2017-07-25 00:06:48 +0100

    Photos from camera don't provide a URL. Work out how to save, then pull location.

--- Recce/ViewController.swift
+++ Recce/ViewController.swift
@@ -19,12 +19,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     
-    
-    let UIImagePickerControllerImageURL: String = ""
-    let UIImagePickerControllerPHAsset: String = ""
-    
     // Get the longitude and latitude
     let manager = CLLocationManager()
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
         let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
@@ -51,102 +48,62 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         self.present(image, animated: true)
     }
     
+    func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
     
-    // SAVE IMAGE TO PHOTO LIBRARY
+    let UIImagePickerControllerImageURL: String = ""
+
+    
+
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         
-        func addAsset(image: UIImage, location: CLLocation? = nil) {
-            PHPhotoLibrary.shared().performChanges({
-                // Request creating an asset from the image.
-                let creationRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
-                let asset: AnyObject? = nil
-                // Set metadata location
-                if let location = location {
-                    creationRequest.location = location
-                    
-                }
-            }, completionHandler: { success, error in
-                if !success {
-                    NSLog("error creating asset: \(String(describing: error))")
-                } else {
-                    //print(info[UIImagePickerControllerMediaMetadata]!)
-//                    var long:Double = location!.coordinate.longitude
-//                    var lat:Double = location!.coordinate.latitude
-//                    //var location = long + " " + lat
-//                    let location = "\(long) + \(lat)"
-//                    var locationDisplay = "\(location)"
-//                    
-//                    return locationDisplay
-                    }
-            })
-        }
+        let here = "none"
         
-        addAsset(image: image, location: manager.location)
+        PHPhotoLibrary.shared().performChanges({
+            let assetRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
+            let here = assetRequest.location
+            print(here)
+        }, completionHandler: { success, error in
+            print(here)
+        })
         
-        
+        print(here)
         var chosenImage:UIImage?
         var location = "Not known"
         var timeTaken = "Not known"
-        print(UIImagePickerControllerImageURL)
-        if let URL = info[UIImagePickerControllerImageURL] as? URL {
-            print("We got the URL as \(URL)")
-            let opts = PHFetchOptions()
-            opts.fetchLimit = 1
-            let assets = PHAsset.fetchAssets(withALAssetURLs: [URL], options: opts)
-            for assetIndex in 0..<assets.count {
-                let asset = assets[assetIndex]
-                location = String(describing: asset.location)
-                timeTaken = asset.creationDate!.description
-                
-                CLGeocoder().reverseGeocodeLocation(asset.location!, completionHandler: {(placemarks, error) -> Void in
-                    print(location)
-                    
-                    if let error = error {
-                        print("Reverse geocoder failed with error" + error.localizedDescription)
-                        return
-                    }
-                    
-                    if placemarks != nil && placemarks!.count > 0 {
-                        DispatchQueue.main.async {
-                            self.geoLabel.text = placemarks![0].locality!
-                        }
-                    } else {
-                        print("There's a problem with the data, motherfucker")
-                    }
-                })
-            }
-        }
-        
-        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
-            chosenImage = editedImage
-        } else if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            chosenImage = selectedImage
-        }
         
         dismiss(animated: true) {
             DispatchQueue.main.async {
                 self.myImageView.image = chosenImage
-                self.locationLabel.text = locationDisplay
+                self.locationLabel.text = location
                 self.timeLabel.text = timeTaken
             }
         }
+        
+        //let imageURL = info[UIImagePickerControllerImageURL] as? URL
+////        if url != nil {
+////            
+////            let fetchResult = PHAsset.fetchAssets(withALAssetURLs: [url!], options: nil)
+////            let asset = fetchResult.firstObject
+////            print(asset?.location?.coordinate.latitude)
+////            print(asset?.creationDate)
+////        }
+        
     }
 
-    
-//    func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
-//        if let error = error {
-//            // we got back an error!
-//            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
-//            ac.addAction(UIAlertAction(title: "OK", style: .default))
-//            present(ac, animated: true)
-//        } else {
-//            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
-//            ac.addAction(UIAlertAction(title: "OK", style: .default))
-//            present(ac, animated: true)
-//        }
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -158,12 +115,109 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         manager.startUpdatingLocation()
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+        
+//        dismiss(animated: true) {
+//            DispatchQueue.main.async {
+//                self.myImageView.image = chosenImage
+//                self.locationLabel.text = location
+//                self.timeLabel.text = timeTaken
+//            }
+//        }
+
+    // SAVE IMAGE TO PHOTO LIBRARY
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        
+//        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+//        
+//        let imageURL = info[UIImagePickerControllerReferenceURL] as! NSURL
+//        self.imageFromAsset(imageURL)
+//    }
+//    
+//        func addAsset(image: UIImage, location: CLLocation? = nil) {
+//            PHPhotoLibrary.shared().performChanges({
+//                // Request creating an asset from the image.
+//                let creationRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
+//                let asset: AnyObject? = nil
+//                // Set metadata location
+//                if let location = location {
+//                    creationRequest.location = location
+//                    
+//                }
+//            }, completionHandler: { success, error in
+//                if !success {
+//                    NSLog("error creating asset: \(String(describing: error))")
+//                } else {
+//                    //print(info[UIImagePickerControllerMediaMetadata]!)
+//                    var long:Double = location!.coordinate.longitude
+//                    var lat:Double = location!.coordinate.latitude
+//                    //var location = long + " " + lat
+//                    let location = "\(long) + \(lat)"
+//                    var locationDisplay = "\(location)"
+//                    
+//                    return locationDisplay
+//                 }
+//            })
+//        }
+//        
+//        addAsset(image: image, location: manager.location)
+//        
+//        
+//        var chosenImage:UIImage?
+//        var location = "Not known"
+//        var timeTaken = "Not known"
+//        print(UIImagePickerControllerImageURL)
+//        if let URL = info[UIImagePickerControllerImageURL] as? URL {
+//            print("We got the URL as \(URL)")
+//            let opts = PHFetchOptions()
+//            opts.fetchLimit = 1
+//            let assets = PHAsset.fetchAssets(withALAssetURLs: [URL], options: opts)
+//            for assetIndex in 0..<assets.count {
+//                let asset = assets[assetIndex]
+//                location = String(describing: asset.location)
+//                timeTaken = asset.creationDate!.description
+//                
+//                CLGeocoder().reverseGeocodeLocation(asset.location!, completionHandler: {(placemarks, error) -> Void in
+//                    print(location)
+//                    
+//                    if let error = error {
+//                        print("Reverse geocoder failed with error" + error.localizedDescription)
+//                        return
+//                    }
+//                    
+//                    if placemarks != nil && placemarks!.count > 0 {
+//                        DispatchQueue.main.async {
+//                            self.geoLabel.text = placemarks![0].locality!
+//                        }
+//                    } else {
+//                        print("There's a problem with the data, motherfucker")
+//                    }
+//                })
+//            }
+//        }
+    
+//        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+//            chosenImage = editedImage
+//        } else if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+//            chosenImage = selectedImage
+//        }
+//        
+//        dismiss(animated: true) {
+//            DispatchQueue.main.async {
+//                self.myImageView.image = chosenImage
+//                self.locationLabel.text = location
+//                self.timeLabel.text = timeTaken
+//            }
+//        }
+//    }
+
+
 
